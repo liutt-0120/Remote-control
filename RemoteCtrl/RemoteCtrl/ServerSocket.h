09064f30a97a1f7441d528ca44712e2dd920c69a
SuXian <727039915@qq.com>
2023-04-20 21:02:25 +0800

    1.隐藏命令行窗口，后台运行 2.设计传输数据用的包

--- RemoteCtrl/RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/RemoteCtrl/ServerSocket.h
@@ -3,6 +3,80 @@
 #include "pch.h"
 #include "framework.h"
 
+#define MAX_SIZE 4096
+
+class CPacket {
+public:
+	CPacket():sHead(0),nLength(0),sCmd(0),sSum(0){}
+	CPacket(const CPacket& pack) {
+		sHead = pack.sHead;
+		nLength = pack.nLength;
+		sCmd = pack.sCmd;
+		strData = pack.strData;
+		sSum = pack.sSum;
+	}
+	CPacket& operator=(const CPacket& pack) {
+		if (this != &pack) {
+			sHead = pack.sHead;
+			nLength = pack.nLength;
+			sCmd = pack.sCmd;
+			strData = pack.strData;
+			sSum = pack.sSum;
+		}
+		return *this;
+	}
+	CPacket(const BYTE* pData, size_t& nSize) {
+		size_t i = 0;
+		//查找包头
+		for (; i < nSize; ++i) {
+			if (*(WORD*)(pData + i) == 0xFEFF) {
+				sHead = *(WORD*)(pData + i);
+				i += 2;
+				break;
+			}
+		}
+		if (i + 4 + 2 + 2 > nSize) {	//相当于是若这个包只有长度(4)、命令(2)、校验(2)或其中部分，那没数据啊，没必要解析；或有数据但包协议不完整，也不校验
+			nSize = 0;
+			TRACE("%s(%d):%s,解析失败:%s\n", __FILE__, __LINE__, __FUNCTION__, "");
+			return;
+		}
+		//获取长度
+		nLength = *(DWORD*)(pData + i); i += 4;
+		if (nLength + i > nSize || nLength <= 4) {		//前者意味着这次传输的包未完全接收到，不完整；后者意味着要么包里没数据，要么包不完整，都没必要解析
+			nSize = 0;
+			TRACE("%s(%d):%s,解析失败:%s\n", __FILE__, __LINE__, __FUNCTION__, "");
+			return;
+		}
+		//获取命令
+		sCmd = *(WORD*)(pData + i); i += 2;
+		//获取数据
+		strData.resize(nLength - 2 - 2);
+		memcpy((void*)strData.c_str(), pData + i, nLength - 4);	//将包内数据传给strData
+		i += nLength - 4;
+		//获取校验
+		sSum = *(WORD*)(pData + i); i += 2;
+		//验证校验，这块的和校验只将数据求和
+		WORD sum = 0;
+		for (size_t j = 0; j < strData.size(); j++) {
+			sum += BYTE(strData[j]) & 0xFF;
+		}
+		if (sum == sSum) {
+			nSize = i;	//长度值（从控制命令开始，到和校验结束）+包头(2)+长度(4) = 包长度
+			return;
+		}
+		nSize = 0;
+		return;
+	}
+	~CPacket(){}
+public:
+	WORD sHead;		//包头，固定为0xFEFF
+	DWORD nLength;	//长度（从控制命令开始，到和校验结束）
+	WORD sCmd;		//控制命令
+	std::string strData;	//数据
+	WORD sSum;		//和校验
+};
+
+
 ///// <summary>
 ///// 方式一：这个类的目的只是项目最初Startup和项目结尾Cleanup套接字环境，而以这种开放的形式是难以保证这个类在项目中不被实例化。
 ///// </summary>
@@ -31,10 +105,10 @@
 //};
 //extern CServerSocket server;
 
+
 //-----------------------------------------------------------------------------------------
 //确保唯一性就要使用单例模式：使类的一个对象成为系统中的唯一实例
 //-----------------------------------------------------------------------------------------
-
 
 ///// <summary>
 ///// 方式二：
@@ -88,7 +162,6 @@
 * 利用这个特征，可以在单例类中定义一个这样的静态成员，它的唯一工作就是在析构函数中删除单例类的实例。
 * //https://blog.csdn.net/realxie/article/details/7090493
 */
-
 class CServerSocket {
 public:
 	static CServerSocket* getInstance() {
@@ -123,23 +196,34 @@ public:
 		return true;
 	}
 
-	bool ForRecv() {
-		if (m_sockCli == -1)return false;
-		char recvbuf[1024] = "";
+	int DealCommand() {
+		if (m_sockCli == -1)return -1;
+		//char recvbuf[1024] = "";
+		char* recvBuf = new char[MAX_SIZE];	//缓冲区较大，存储到堆上
+		memset(recvBuf, 0, MAX_SIZE);
+		size_t index = 0;
 		while (true) {
-			int len = recv(m_sockCli, recvbuf, sizeof(recvbuf), 0);
+			size_t len = recv(m_sockCli, recvBuf + index, MAX_SIZE - index, 0);	//len --- 本次接收到数据的长度
 			if (len <= 0) {
-				return false;
+				return -1;
+			}
+			index += len;	//修正起始位置
+			len = index;	//实际接收到数据的长度
+			m_packet = CPacket((BYTE*)recvBuf, len);	//len接收实际处理的包的长度
+			if (len > 0) {
+				memmove(recvBuf, recvBuf + len, MAX_SIZE - len);	//将已被处理的包所占的内存位置释放：后面的数据挪前面来
+				index -= len;
+				return m_packet.sCmd;
 			}
 		}
-		return true;
+		return -1;
 	}
 
 	bool ForSend() {
 		if (m_sockCli == -1)return false;
-		char sendbuf[1024] = "";
+		char sendBuf[1024] = "";
 		while (true) {
-			int len = send(m_sockCli, sendbuf, sizeof(sendbuf), 0);
+			int len = send(m_sockCli, sendBuf, sizeof(sendBuf), 0);
 			if (len <= 0) {
 				return false;
 			}
@@ -149,6 +233,7 @@ public:
 private:
 	SOCKET m_sockSrv;
 	SOCKET m_sockCli;
+	CPacket m_packet;
 	CServerSocket(){
 		m_sockSrv = INVALID_SOCKET;
 		m_sockCli = INVALID_SOCKET;
